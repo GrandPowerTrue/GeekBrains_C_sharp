6903979b6b74b291ad87ea1c7453bfebfb4fa63e
Pavel.Zhelikhov <paul_mailbox@mail.ru>
2023-07-04 00:33:56 +0300

    Выполнил домашнее задание к 6 семинару

---
+++ 06_HomeWork/Task_41/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+void PrintSingleArray(int[] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+    Console.Write($"{array[i]}\t");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+Console.WriteLine("-------------------------------------------------------------");
+Console.WriteLine("Обозначьте количество анализируемых чисел m (размер массива): ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("-------------------------------------------------------------");
+
+int[] Array = new int[m];
+Console.WriteLine($"Введите {m} чисел для заполнения массива: ");
+for (int i = 0; i < m; i++)
+{
+Array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine("-------------------------------------------------------------");
+Console.WriteLine($"Имеем массив из {m} чисел для исследования: ");
+Console.WriteLine("-------------------------------------------------------------");
+PrintSingleArray(Array);
+Console.WriteLine("-------------------------------------------------------------");
+
+int count = 0;
+for (int i = 0; i < m; i++)
+{
+    if (Array[i] > 0)
+    {
+        count = count + 1;
+    }
+}
+
+if (count > 0)
+    {
+    Console.WriteLine("-------------------------------------------------------------");
+    Console.WriteLine($"Введено чисел больше нуля {count}");
+    Console.WriteLine("-------------------------------------------------------------");
+    }
+    else
+        {
+        Console.WriteLine("-------------------------------------------------------------");
+        Console.WriteLine("В массиве нет чисел больше нуля");
+        Console.WriteLine("-------------------------------------------------------------"); 
+        }
---
+++ 06_HomeWork/Task_43/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями
+// y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine();
+Console.WriteLine("-------------------------------------------------------------");
+Console.WriteLine("Введите значение углового коэффициента k1 для первой прямой: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение пересечения первой прямой b1 с осью y: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine("Введите значение углового коэффициента k2 для второй прямой: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение пересечения второй прямой b2 с осью y: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("-------------------------------------------------------------");
+
+if (k1 == k2)
+{
+    Console.WriteLine("-------------------------------------------------------------");
+    Console.WriteLine("Прямые параллельны и не могут пересекаться");
+    Console.WriteLine("-------------------------------------------------------------");
+}
+    else
+        {
+            double x = (b2 - b1) / (k1 - k2);
+            double y = k1 * ((b2 - b1)/(k1 - k2)) + b1;
+            Console.WriteLine();
+            Console.WriteLine($"Координаты точки пересечения прямых ({x:F2} ; {y:F2})");
+            Console.WriteLine("-------------------------------------------------------------");
+            Console.WriteLine();
+        }
